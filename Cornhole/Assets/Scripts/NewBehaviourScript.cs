49dcac2983f87a7dfd4ecac018a0fa111f9c78fb
tjm2285 <tjm2285@yahoo.com>
2019-05-24 09:17:52 -0700

    added unity project

---
+++ Cornhole/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    string _value = "Hello World";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(_value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Cornhole/Cornhole/Assets/Scripts/ProjectileShooter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShooter : MonoBehaviour
+{
+
+    private GameObject _prefab;
+    private Vector3 _direction;
+    private bool isMouseDown = false;
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _prefab = Resources.Load<GameObject>("projectile");
+        _direction = Camera.main.transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMouseDown = true;
+
+        }
+
+
+        if(isMouseDown)
+        {
+            speed+=0.2f;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log(speed);
+            GameObject projectile = Instantiate<GameObject>(_prefab);
+
+            projectile.transform.position = transform.position + _direction * 2;
+
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = _direction * speed;
+            speed = 0;
+            isMouseDown = false;
+        }
+    }
+}
